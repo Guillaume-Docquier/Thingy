9c76145a8f1dcd0453e2cc4f40153ad19a30064d
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-04-15 10:14:17 +0200

    Improved the interactivity of the page when adding thingies

--- static/javascript/authentication/services/authentication.service.js
+++ static/javascript/authentication/services/authentication.service.js
@@ -26,7 +26,7 @@
 
       function loginSuccessFn(email){
         console.log('Logged in as ' + email);
-        setAuthenticatedAccount({email:email});
+        setAuthenticatedAccount({email:email, user:'Guillaume Docquier'});
         return true;
         //window.location = '/';
       }
@@ -67,10 +67,10 @@
    * @memberOf thinkster.authentication.services.Authentication
    */
     function getAuthenticatedAccount() {
-      if (!$cookies.authenticatedAccount) {
+      if (!$cookies.get('authenticatedAccount')) {
         return;
       }
-      return JSON.parse($cookies.authenticatedAccount);
+      return $cookies.getObject('authenticatedAccount');
     }
 
     /**
@@ -80,7 +80,7 @@
    * @memberOf thinkster.authentication.services.Authentication
    */
     function isAuthenticated() {
-      return !!$cookies.authenticatedAccount;
+      return !!$cookies.get('authenticatedAccount');
     }
 
     /**
@@ -91,7 +91,9 @@
    * @memberOf thinkster.authentication.services.Authentication
    */
     function setAuthenticatedAccount(account) {
-      $cookies.authenticatedAccount = JSON.stringify(account);
+      var now = Date.now(),
+      exp = new Date(now + (24*60*60*1000)); // a day from now
+      $cookies.putObject('authenticatedAccount', account, {expires:exp});
     }
 
     /**
@@ -101,7 +103,7 @@
    * @memberOf thinkster.authentication.services.Authentication
    */
     function unauthenticate() {
-      delete $cookies.authenticatedAccount;
+      $cookies.remove('authenticatedAccount');
     }
   }
 })();
--- static/javascript/posts/controllers/thingies.controller.js
+++ static/javascript/posts/controllers/thingies.controller.js
@@ -16,16 +16,47 @@
     vm.create = create;
     vm.remove = remove;
 
+    activate();
+
+    function activate() {
+      // Set the owned property
+      if(Authentication.isAuthenticated())
+      {
+        var auth = Authentication.getAuthenticatedAccount();
+        for(var i = 0; i < vm.numPosts; i++)
+        {
+          if(vm.posts[i].user == auth.user) vm.posts[i].owned = true;
+        }
+      }
+    }
+
     function get(){
       vm.posts = Thingies.get(vm.getuser);
+      updateNumPosts();
     }
 
     function create(){
+      // Create post
       vm.posts = Thingies.create(vm.user, vm.title, vm.description);
+      // Update number of posts
+      updateNumPosts();
+      // Make sure to set the owned property
+      vm.posts[vm.numPosts-1].owned = true;
+      // Reset the form
+      resetForms();
     }
 
     function remove(){
       vm.posts = Thingies.remove(vm.thingyId);
+      updateNumPosts();
+    }
+
+    function updateNumPosts() {
+      vm.numPosts = vm.posts.length;
+    }
+
+    function resetForms() {
+      vm.title = vm.description = vm.user = vm.getuser = "";
     }
   }
 })();
