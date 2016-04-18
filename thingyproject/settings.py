acc83b15d6ea962b9f98e6f2cbc0b758c55cc958
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-04-18 13:46:15 +0200

    Fixed new urls (/users/username)

--- thingyproject/settings.py
+++ thingyproject/settings.py
@@ -142,7 +142,5 @@ AUTH_USER_MODEL = 'authentication.Account'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    
 }
