c34d643cea5f53cc2d0fc26e54997b65c265f261
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-04-18 11:29:42 +0200

    Included browsable api

--- thingyproject/settings.py
+++ thingyproject/settings.py
@@ -138,3 +138,11 @@ STATICFILES_FINDERS = (
 )
 
 AUTH_USER_MODEL = 'authentication.Account'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- thingyproject/urls.py
+++ thingyproject/urls.py
@@ -21,9 +21,10 @@ accounts_router = routers.NestedSimpleRouter(
 accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/', include(accounts_router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
-    url('^.*$', IndexView.as_view(), name='index'),
+    url(r'^.*$', IndexView.as_view(), name='index'),
 ]
