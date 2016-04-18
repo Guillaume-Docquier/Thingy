95ac692c2b72afd5093f7fafd9556b129faa19a9
Alexander <alexander.groth92@gmail.com>
2016-04-18 17:28:31 +0200

    populate-func for cat and reg updated

---
+++ populate_db_cat.py
@@ -0,0 +1,48 @@
+import os, sys 
+#from thingy
+#print(settings.BASE_DIR)
+script_path = '/home/alexandergroth/Desktop/Thingy-github'
+
+project_dir = os.path.abspath(os.path.join(script_path,'thingyproject' ))
+sys.path.insert(0, project_dir)
+#os.environment.setdefault('DJANGO_SETTINGS_MODULE', 'thingyproject.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'thingyproject.settings'
+
+import django 
+django.setup()
+
+from posts.models import Category, Subcategory
+
+def populate():
+	category_list = ["Fordon", "Hemmasaker", "Kladsel", "Evenemang", "Hobby", "Bocker", "ovrigt"]
+	subcategory_list = [
+["Bilar", "Reservdelar", "Verktyg", "Cyklar", "Husvagnar", "Batar", "Motorcyklar", "Skotrar", "ovrigt"],
+["Malning", "Snickra", "Tradgardsverktyg", "Mobler","ovrigt"],
+["Finklader herr", "Finklader dam", "ovrigt"],
+["Koksutrustning", "Ljud", "Bild", "Ljus", "Mobler", "ovrigt"],
+["Sportutrustning", "Fritidsutrustning", "Instrument", "Fiske", "Leksaker", "Jakt", "ovrigt"],
+["Kurslitteratur", "Reseguider", "ovrigt"],
+["ovrigt"]
+]
+
+	for i in range(len(category_list)):
+		category = add_category(category_list[i])
+		for elem in subcategory_list[i]:
+			add_subcategory(cat=category, name=elem)
+
+
+def add_category(category_name):
+	c = Category.objects.get_or_create(cname=category_name)[0]	
+	c.save()
+	#print (c)
+	return c
+
+def add_subcategory(cat, name):
+	s = Subcategory.objects.get_or_create(category=cat, sub_cat_name=name)[0]
+	
+	s.save()
+if __name__ == '__main__':
+	populate()
+	
+		 
+
