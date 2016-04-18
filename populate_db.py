67f930b1c964258cbbfa51f6e19bf0e2d173da50
Alexander <alexander.groth92@gmail.com>
2016-04-18 16:05:17 +0200

    populate-func and Posts columns updated

--- populate_db.py
+++ populate_db.py
@@ -1,18 +1,38 @@
-import os
-os.environment.setdefault('DJANGO_SETTINGS_MODULE', 'thingyproject.settings')
+import os, sys 
+#from thingy
+#print(settings.BASE_DIR)
+script_path = '/home/alexandergroth/Desktop/Thingy-github'
+
+project_dir = os.path.abspath(os.path.join(script_path,'thingyproject' ))
+sys.path.insert(0, project_dir)
+#os.environment.setdefault('DJANGO_SETTINGS_MODULE', 'thingyproject.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'thingyproject.settings'
+
+import django 
+django.setup()
 
 from posts.models import Region, Town
 
 def populate():
-	region_list = []
-	town_list = []
+	region_list = ["Uppsala", "STockholm"]
+	town_list = [["Uppsala", "Tirep", "Rimbo"],["Taby", "Sollentuna", "Vasby"]]
 
 	for i in range(len(region_list)):
-		add_region(region_list[i])
+		region = add_region(region_list[i])
 		for elem in town_list[i]:
-			add_town(reg=region_list[i], town=elem)
+			add_town(reg=region, name=elem)
 
-if __name __ == '__main__'
+def add_region(region_name):
+	r = Region.objects.get_or_create(name=region_name)[0]	
+	r.save()
+	#print (r)
+	return r
+
+def add_town(reg, name):
+	t = Town.objects.get_or_create(region=reg, name=name)[0]
+	
+	t.save()
+if __name__ == '__main__':
 	populate()
 
 	
--- thingyproject/settings.py
+++ thingyproject/settings.py
@@ -139,10 +139,4 @@ STATICFILES_FINDERS = (
 
 AUTH_USER_MODEL = 'authentication.Account'
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+
