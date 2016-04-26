be47bebbefbe3e1b310bb53e5e9487dbd2661455
Alexander <alexander.groth92@gmail.com>
2016-04-26 10:43:16 +0200

    From filip with love

--- posts/filters.py
+++ posts/filters.py
@@ -6,9 +6,23 @@ from rest_framework import filters
 from rest_framework import generics
 
 class PostFilter(filters.FilterSet):
-    min_price = django_filters.NumberFilter(name="price", lookup_type='gte')
-    max_price = django_filters.NumberFilter(name="price", lookup_type='lte')
+    #min_price = django_filters.NumberFilter(name="price", lookup_type='gte')
+    #max_price = django_filters.NumberFilter(name="price", lookup_type='lte')
+    #title = django_filters.CharFilter( )
+
+    # filter_overrides = {
+    #     Post.CharField: {
+    #         'filter_class': django_filters.CharFilter,
+    #         'extra': lambda f: {
+    #             'lookup_expr': 'icontains',
+    #         },
+    #     }
+    # }
+
     class Meta:
         model = Post
-        fields = ['title', 'min_price', 'max_price']
+        fields =  {
+            'title': ['icontains']}
+
+       # , 'min_price', 'max_price'}
 
