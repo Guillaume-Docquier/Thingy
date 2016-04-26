e27607453c6e5e7bb237348caea3136871480bbf
Alexander <alexander.groth92@gmail.com>
2016-04-26 10:11:22 +0200

    trying to restore after fuck up

--- posts/filters.py
+++ posts/filters.py
@@ -1,11 +1,14 @@
+import django_filters
 from django.contrib.auth.models import User
 from posts.models import Post
 from posts.serializers import PostSerializer
 from rest_framework import filters
 from rest_framework import generics
 
-class UserListView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer = PostSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = ('title')
+class PostFilter(filters.FilterSet):
+    min_price = django_filters.NumberFilter(name="price", lookup_type='gte')
+    max_price = django_filters.NumberFilter(name="price", lookup_type='lte')
+    class Meta:
+        model = Post
+        fields = ['title', 'min_price', 'max_price']
+
--- thingyproject/settings.py
+++ thingyproject/settings.py
@@ -40,13 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication',
     'posts',
-    'crispy_forms',
+    #'crispy_forms',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
-
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
+#
+# }
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
