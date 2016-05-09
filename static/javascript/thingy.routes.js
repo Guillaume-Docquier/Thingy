9016f0088e6710700e97c73759f4a06cae6aedff
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-05-09 14:27:26 +0200

    Updated routing.

--- static/javascript/thingy.routes.js
+++ static/javascript/thingy.routes.js
@@ -50,19 +50,25 @@
       controllerAs: 'vm',
       templateUrl: '/static/templates/profiles/settings.html'
     })
-    // User Thingies
+    // User messages
+    .when('/users/:username/messages', {
+      controller: 'MessagesController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/profiles/messages.html'
+    })
+    // TODO User Thingies
     .when('/users/:username/thingies', {
       controller: 'ProfileController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/profiles/thingies.html'
     })
-    // User rents
+    // TODO User rents
     .when('/users/:username/rents', {
       controller: 'ProfileController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/profiles/rents.html'
     })
-    // User reviews
+    // TODO User reviews
     .when('/users/:username/reviews', {
       controller: 'ProfileController',
       controllerAs: 'vm',
