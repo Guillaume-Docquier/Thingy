f44dcb10a2287ee6418c52df15b0f70306c119ed
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-05-09 14:13:42 +0200

    Hotfix of the routing.

--- static/javascript/thingy.routes.js
+++ static/javascript/thingy.routes.js
@@ -27,7 +27,7 @@
       reloadOnSearch: false
     })
     // Thingy detailed
-    .when('/thingies/detailed/:thingyid', {
+    .when('/thingies/details/:thingyid', {
       controller: 'ThingyController',
       controllerAs: 'vm',
       templateUrl: '/static/templates/posts/thingy.html',
