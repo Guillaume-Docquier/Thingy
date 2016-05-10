6201d63b6a9adb3f7edba9be13d75977a7a1b850
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-05-10 11:52:39 +0200

    Calendar test.

--- static/javascript/posts/controllers/thingy.controller.js
+++ static/javascript/posts/controllers/thingy.controller.js
@@ -5,15 +5,17 @@
     .module('thingy.posts.controllers')
     .controller('ThingyController', ThingyController);
 
-  ThingyController.$inject = ['Posts', '$routeParams', 'Authentication'];
+  ThingyController.$inject = ['Posts', '$routeParams', 'Authentication', '$scope'];
 
-  function ThingyController(Posts, $routeParams, Authentication) {
+  function ThingyController(Posts, $routeParams, Authentication, $scope) {
     var vm = this;
 
     // Functions and Data
     vm.rent = rent;
     vm.profile = '';
     vm.post = '';
+    vm.uiConfig = '';
+    vm.eventSources = {};
 
     // Bindings, empty string to prevent unwanted behaviour
     vm.dates = [];
@@ -25,6 +27,45 @@
 
       Posts.getSinglePost(thingyId).then(postSuccessFn, postErrorFn);
       vm.profile = Authentication.getAuthenticatedAccount();
+      // Calendar
+      vm.uiConfig = {
+        calendar: {
+          height: 450,
+          editable: true,
+          header: {
+            left: 'month basicWeek',
+            center: 'title',
+            right: 'today prev,next'
+          },
+          dayClick: $scope.alertEventOnClick,
+          eventDrop: $scope.alertOnDrop,
+          eventResize: $scope.alertOnResize
+        }
+      };
+      vm.eventSources = {
+        events: [
+          {
+            title: 'Unavailable',
+            start: '2016-05-12',
+            end: '2016-05-17',
+            rendering: 'background',
+          },
+          {
+            title: 'Unavailable',
+            start: '2016-05-29',
+            end: '2016-06-01',
+            rendering: 'background',
+          },
+          {
+            title: 'Unavailable',
+            start: '2016-06-09',
+            end: '2016-06-24',
+            rendering: 'background',
+          },
+        ],
+        color: 'red',
+        allDayDefault: 'true'
+      };
 
       function postSuccessFn(data, status, headers, config) {
         vm.post = data.data;
