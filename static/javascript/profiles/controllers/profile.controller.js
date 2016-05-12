3fbb19f87f83616adf07d93abf8aec860b39f4d2
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-05-12 11:02:00 +0200

    Wired received messages.

--- static/javascript/profiles/controllers/profile.controller.js
+++ static/javascript/profiles/controllers/profile.controller.js
@@ -13,18 +13,16 @@
   function ProfileController($scope, $location, $routeParams, Profile, Posts) {
     var vm = this;
 
-    vm.profile = undefined;
-    vm.messages = [
-      {
-        author: {username:'gudoc'},
-        body: 'Wassup bro'
-      },
-      {
-        author: {username:'gudoc'},
-        body: 'Wrong person'
-      }
-    ];
+    // Functions and data
+    vm.getUnreadMessages = getUnreadMessages;
+    vm.unreadNum = 0;
+    vm.profile = '';
+    vm.receivedMessages = [];
+    vm.sentMessages = [];
     vm.posts = [];
+
+    // Bindings
+
 
     activate();
 
@@ -43,7 +41,6 @@
       }
 
       Profile.get(username).then(profileSuccessFn, profileErrorFn);
-      Posts.getUserPosts(username).then(postsSuccessFn, postsErrorFn);
 
       /**
       * @name profileSuccessProfile
@@ -51,6 +48,9 @@
       */
       function profileSuccessFn(data, status, headers, config) {
         vm.profile = data.data;
+        Profile.getReceivedMessages(vm.profile.id).then(getRSuccessFn, getRErrorFn);
+        //Profile.getSentMessages(vm.profile.id).then(getSSucessFn, getSErrorFn);
+        Posts.getUserPosts(vm.profile.username).then(postsSuccessFn, postsErrorFn);
       }
 
       /**
@@ -60,6 +60,23 @@
       function profileErrorFn(data, status, headers, config) {
         $location.url('/');
         console.log("Error loading profile");
+      }
+
+      /**
+      * @name getRSuccessFn
+      * @desc Update 'receivedMessages' on viewmodel
+      */
+      function getRSuccessFn(data, status, headers, config) {
+        vm.receivedMessages = data.data.messages;
+        vm.getUnreadMessages();
+      }
+
+      /**
+      * @name getRErrorFn
+      * @desc Log error in the console
+      */
+      function getRErrorFn(data, status, headers, config) {
+        console.log("Error loading received messages");
       }
 
       /**
@@ -76,6 +93,14 @@
         */
       function postsErrorFn(data, status, headers, config) {
         console.log("Error loading Thingies");
+      }
+    }
+
+    function getUnreadMessages() {
+      vm.unreadNum = 0;
+      for(var i = 0; i < vm.receivedMessages.length; i++) {
+        if(vm.receivedMessages[i].unread)
+          vm.unreadNum++;
       }
     }
   }
