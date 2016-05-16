c88c3c6863e9078b8698f24a666243acf7186496
Guillaume Docquier <guillaumedocquier@hotmail.com>
2016-05-16 17:50:25 +0200

    Added region to home search.

--- static/javascript/home/controllers/index.controller.js
+++ static/javascript/home/controllers/index.controller.js
@@ -5,20 +5,37 @@
     .module('thingy.home.controllers')
     .controller('IndexController', IndexController);
 
-  IndexController.$inject = ['$scope'];
+  IndexController.$inject = ['$scope', 'Posts'];
 
-  function IndexController($scope) {
+  function IndexController($scope, Posts) {
     var vm = this;
 
     // Functions and data
     vm.search = search;
+    vm.regions = [];
 
     // Bindings
     // Empty strings to prevent unwanted behaviour due to undefined values
     vm.searchTerm = '';
+    vm.region = '';
+
+    activate();
+
+    function activate() {
+      Posts.getAllRegions().then(regionsSuccessFn, regionsErrorFn);
+
+      function regionsSuccessFn(data) {
+        vm.regions = data.data;
+      }
+
+      function regionsErrorFn(data) {
+        alert('Could not load regions.');
+        console.error('Error: ' + JSON.stringify(data.data));
+      }
+    }
 
     function search(advanced){
-      window.location = '/thingies/search?advanced=' + advanced + '&search=' + vm.searchTerm;
+      window.location = '/thingies/search?advanced=' + advanced + '&search=' + vm.searchTerm + '&region=' + vm.region.name;
     }
   }
 })();
